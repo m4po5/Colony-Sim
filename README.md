111f663ffa7b00c8644a64eb5cd29950ec9b565d
m4po5 <j4.m4po5@gmail.com>
2025-09-07 17:34:49 +0200

    citizen info view overhaul

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Colony-Sim
 ## Roadmap
 - [ ] UI Overhaul
+  - [x] Merge Task Progress into minimalized Citizin View
+  - [ ] Blackboard per Location
+  - [ ] Blackboard and Management Tabs per Location
 - [ ] Construct Building Task: Design and Implementation
 - [ ] Need Food Update
 - [ ] Deterioration Of Surplus Wares
---
+++ ui.js
@@ -0,0 +1,116 @@
+
+// ----------- Game View and Main Coil ---------
+
+function updateSettlementView(){
+
+    updateStorage();
+    updateCitizensView()
+
+    const locationsView = $(`#locations`);
+    $(locationsView).empty();
+    locationsView.append(createLocationView(pieces.settlement));
+    pieces.settlement.buildings.forEach(location => {
+        locationsView.append(createLocationView(location));
+    })
+}
+
+function updateStorage(){
+    $("#wood").text(pieces.settlement.storage.stored.wood);
+    $("#food").text(pieces.settlement.storage.stored.food);
+}
+
+const View={
+    citizens:[]
+}
+
+function updateCitizensView(){
+    const citizensView = $("#citizens");
+
+    function updateViewCitizensPairs(citizensView) {
+        const citizens = pieces.settlement.citizens;
+        if (View.citizens.length === 0 && citizens != 0) {
+                createCitizenViewPair(citizensView,citizens[0]);
+        }
+        pieces.settlement.citizens.forEach(citizen => {
+            if (View.citizens.filter(vc => vc.citizenData === citizen).length === 0) {
+                createCitizenViewPair(citizensView,citizen);
+            }
+        });
+        function createCitizenViewPair(citizensView,citizen) {
+            const citView = createCitizenView(citizen);
+            View.citizens.push({ citizenData: citizen, viewId: "#citizenId"+citizen.citId });
+            citizensView.append(citView);
+        }
+        View.citizens.forEach(vc => {
+            if(pieces.settlement.citizens.filter(cit => cit === vc.citizenData).length === 0){
+                $(vc.viewId).remove();
+                const index = View.citizens.indexOf(vc);
+                if(index > -1){
+                    View.citizens.splice(index,1);
+                }
+            }
+        });
+    };
+    updateViewCitizensPairs(citizensView)
+    
+    // update displayed data
+    View.citizens.forEach((vc) => {
+        let taskText = `<div class="progress">idling</div>`;
+        if(vc.citizenData.tasks.length > 0){
+            const task = vc.citizenData.tasks[0];
+            taskText = getTaskProgressView(task);
+        }
+        $(vc.viewId).children(".nameplate").children(".task").html(taskText)
+    });
+}
+
+function createCitizenView(citizen){
+    let html = `<div id="citizenId`+citizen.citId+`" class="citizen">`;
+    html += `<div src="citIcon.jpg" class="citIcon"></div>`
+    html += `<div class="nameplate">`
+    html += `<span class="name">`+citizen.name+`</span>`;
+    
+    html += `<div class="task">`
+    if (citizen.tasks.length > 0){
+        let task = citizen.tasks[0];
+        html += getTaskProgressView(task);
+    } else {
+        html += `<div class="progress">idling</div>`;
+    }
+    html += `</div></div>` //nameplate and task
+
+    return html+"</div>"; //citizen div
+}
+
+function getTaskProgressView(task){
+    let html = `<div class="progress">`+task.name;
+    html += `<div class="progressbar `;
+        switch (task.status){
+            case "In Progress":
+                let progress = task.ticksDone/task.ticksToComplete;
+                html += `inProgress" style="width:`+ progress*100 +`%">`;
+                break
+            case "Done":
+                html += `done">`;
+                break;
+            default:
+                html += `todo">`;       
+        }
+    return html+= task.name+"</div></div>";
+}
+
+function createLocationView(location){
+    let html = `<div class="location">`;
+    html += `<div class="name">`+location.name+`</div>`;
+
+    location.taskManagement.tasks.forEach(task =>{
+        html += `<div class="task"><div class="progress">`+task.name+`</div>`;
+        if(task.assignee !== undefined){
+            html += `<div class="assignee">`+task.assignee.name+`</div>`
+        }
+
+        html +=`</div>`;
+    })
+
+    return html+"</div>";
+}
